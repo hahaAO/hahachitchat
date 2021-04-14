7f84c1c2b08c4a110c0aa30165d49d0d298e4faa
hahaAO <vgdq123@outlook.com>
2021-04-14 21:56:05 +0800

    日志名字更正

--- logset.go
+++ logset.go
@@ -25,5 +25,5 @@ func init() {
 	DBlog = log.New(logfile, "DBLOG", log.Ldate|log.Ltime)
 	Serverlog = log.New(logfile, "ServerLOG", log.Ldate|log.Ltime)
 	Redislog = log.New(logfile, "RedisLOG", log.Ldate|log.Ltime)
-	Imglog = log.New(logfile2, "RedisLOG", log.Ldate|log.Ltime)
+	Imglog = log.New(logfile2, "ImgLOG", log.Ldate|log.Ltime)
 }
